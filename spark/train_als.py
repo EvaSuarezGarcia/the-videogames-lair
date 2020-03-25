45f9c12f0c6949d895a96f85433ba398c728aff0
eva.suarez <eva.suarez.garcia@udc.es>
2020-03-25 16:06:29 +0100

    Update ALS training script to support hybrid recommendation
    REF TG-8

--- spark/train_als.py
+++ spark/train_als.py
@@ -11,20 +11,26 @@ def main(argv):
             .config("spark.cassandra.connection.host", FLAGS.cassandraNodes) \
             .config("spark.cassandra.auth.username", FLAGS.cassandraUser) \
             .config("spark.cassandra.auth.password", FLAGS.cassandraPwd) \
+            .config("spark.cassandra.input.consistency.level", "QUORUM") \
             .getOrCreate() \
             as spark:
         spark.sparkContext.setCheckpointDir(FLAGS.checkpointDir)
 
         # Read ratings table as dataframe
-        df = spark.read.format("org.apache.spark.sql.cassandra") \
-            .options(keyspace=FLAGS.keyspace, table=FLAGS.table) \
-            .load()
+        ratings = spark.read.format("org.apache.spark.sql.cassandra") \
+            .options(keyspace=FLAGS.keyspace, table=FLAGS.tables[0]) \
+            .load().select(FLAGS.userCol, FLAGS.itemCol, FLAGS.ratingCol)
+        for table in FLAGS.tables[1:]:
+            table_df = spark.read.format("org.apache.spark.sql.cassandra") \
+                .options(keyspace=FLAGS.keyspace, table=table) \
+                .load().select(FLAGS.userCol, FLAGS.itemCol, FLAGS.ratingCol)
+            ratings = ratings.union(table_df)
 
         # Train ALS model
         als = ALS(maxIter=FLAGS.iter, userCol=FLAGS.userCol, itemCol=FLAGS.itemCol, ratingCol=FLAGS.ratingCol)
         als.setCheckpointInterval(FLAGS.checkpointInterval)
         als.setNumBlocks(FLAGS.numBlocks)
-        model = als.fit(df)
+        model = als.fit(ratings)
 
         # Save model to HDFS
         model.write().overwrite().save(FLAGS.outputPath)
@@ -51,8 +57,9 @@ flags.DEFINE_string("cassandraPwd", None, "Password to access Cassandra")
 flags.mark_flag_as_required("cassandraPwd")
 flags.DEFINE_string("keyspace", None, "Cassandra keyspace")
 flags.mark_flag_as_required("keyspace")
-flags.DEFINE_string("table", None, "Cassandra table")
-flags.mark_flag_as_required("table")
+flags.DEFINE_list("tables", None, "Comma-separated list of Cassandra rating tables")
+flags.mark_flag_as_required("tables")
+flags.register_validator("tables", lambda tables: len(tables) > 0, "Tables list must not be empty")
 
 if __name__ == "__main__":
     app.run(main)
