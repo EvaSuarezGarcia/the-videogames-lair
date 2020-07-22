328c446b2ce07b2f14e4e51f4c452ee50479f329
eva.suarez <eva.suarez.garcia@udc.es>
2020-07-22 17:57:36 +0200

    Remove reference to old secrets.json file

--- videogames_lair_site/videogames_lair_site/settings.py
+++ videogames_lair_site/videogames_lair_site/settings.py
@@ -18,11 +18,8 @@ from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Sensitive settings are hidden
-with open(os.path.join(BASE_DIR, "secrets.json")) as f:
-    secrets = json.load(f)
-
-
 def get_env_value(setting: str):
     try:
         return os.environ[setting]
